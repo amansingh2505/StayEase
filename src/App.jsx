22f2d85df203bc560e66b82e81081acf09634cf1
amansingh2505 <amansinghrajput2352@gmail.com>
2026-07-12 05:50:19 +0530

    Add global scroll restoration for route navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home/Home";
 import HotelDetails from "./pages/HotelDetails/HotelDetails";
 import Wishlist from "./pages/Wishlist/Wishlist";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotel/:id" element={<HotelDetails />} />
         <Route path="/wishlist" element={<Wishlist />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
